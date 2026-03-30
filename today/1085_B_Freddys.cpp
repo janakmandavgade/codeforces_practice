b68e628fc159f30fea59a7871fdd3d67d1649809
janak <janak@forgejo.com>
2026-03-30 17:15:52 +0530

    Formatted 1085_B

--- today/1085_B_Freddys.cpp
+++ today/1085_B_Freddys.cpp
@@ -7,38 +7,45 @@ using namespace std;
 
 typedef long long int ll;
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         // int n;
         // cin>>n;
-        
-        int n, m, l;
-        cin>>n>>m>>l;
 
-        vector<int> v(n+1);
+        int n, m, l;
+        cin >> n >> m >> l;
+
+        vector<int> v(n + 1);
         v[0] = 0;
-        for(int i = 1 ; i <= n ; i++) cin>>v[i];
+        for (int i = 1; i <= n; i++)
+            cin >> v[i];
 
         multiset<ll> s;
 
-        for(int i = 0 ; i < min(m, n+1) ; i++) s.insert(0);
-        int mx = n+1;
-        for(int i = 1 ; i <= n ; i++){
+        for (int i = 0; i < min(m, n + 1); i++)
+            s.insert(0);
+        int mx = n + 1;
+        for (int i = 1; i <= n; i++)
+        {
             mx--;
-            for(int j = v[i-1] ; j < v[i] ; j++){
-                s.insert((*s.begin())+1);
+            for (int j = v[i - 1]; j < v[i]; j++)
+            {
+                s.insert((*s.begin()) + 1);
                 s.erase(s.begin());
             }
             s.erase(--s.end());
-            if(s.size() < min(mx, m)) s.insert(0);
+            if (s.size() < min(mx, m))
+                s.insert(0);
         }
 
         int ans = *(--s.end()) + l - v[n];
-        cout<<ans<<endl;
+        cout << ans << endl;
     }
 
     return 0;

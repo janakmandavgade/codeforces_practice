e053245927117672af7618b16098ef311a9afdf6
Janak Mandavgade <janak2703@gmail.com>
2026-04-07 20:56:07 +0530

    Corrected 1063_A

--- today/1063_A_Souvlaki_vs_Kalamaki.cpp
+++ today/1063_A_Souvlaki_vs_Kalamaki.cpp
@@ -1,37 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-
-        vector<int> v(n);
-        unordered_map<int,int> mpp;
-        int mx_freq = INT_MIN;
-        for(int i = 0 ; i < n ; i++) {
-            cin>>v[i];
-            mpp[v[i]]++;
-            mx_freq = max(mpp[v[i]], mx_freq);
+void solve() {
+    int n; cin >> n;
+    int a[n+1];
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    sort(a+1, a+n+1);
+    for (int i = 2; i <= n - 1; i+=2) {
+        if (a[i] != a[i+1]) {
+            cout << "NO\n";
+            return;
         }
-        
-        if(mx_freq >= (n+1)/2){
-            cout<<"Yes"<<endl;
-        }else{
-            cout<<"No"<<endl;
-        }
-        
     }
+    cout << "YES\n";
+}
 
+int main() {
+    int t; cin >> t;
+    while (t--) solve();
     return 0;
 }

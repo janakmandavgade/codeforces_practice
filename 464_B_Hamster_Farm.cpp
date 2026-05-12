7892263e8f17635af9c8dd34135b78fd8f3af38a
janak <janak@forgejo.com>
2026-05-12 14:10:47 +0530

    Added 464_B

---
+++ 464_B_Hamster_Farm.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+typedef long long int ll;
+
+template <typename t1, typename t2>
+ostream &operator<<(ostream &os, pair<t1, t2> p)
+{
+    return os << "{" << p.first << ", " << p.second << "}";
+}
+
+template <typename t>
+void printVec(vector<t> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+template <typename t>
+void print2dVec(vector<vector<t>> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+    {
+        for (ll j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return;
+}
+
+template <typename t, typename size_t>
+void inputVec(vector<t> &v, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+}
+
+void pLine(ll line)
+{
+    cout << "Reached at Line: " << line << endl;
+}
+
+void printVec(vector<ll> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> v(k);
+    inputVec(v, k);
+
+    ll mn_rem = LLONG_MAX, idx = -1;
+
+    // printVec(v);
+    for (ll i = 0; i < k; i++)
+    {
+        // cout << v[i] << endl;
+        if (mn_rem > (n % v[i]))
+        {
+            mn_rem = (n % v[i]);
+            idx = i;
+        }
+        // cout << mn_rem << " " << n % v[i] << endl;
+    }
+
+    // cout << idx + 1 << " " << ((n / v[idx]) == 0 ? 1 : n / v[idx]) << endl;
+    cout << idx + 1 << " " << n / v[idx] << endl;
+
+    return 0;
+}

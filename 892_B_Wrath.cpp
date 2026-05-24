4923df83775d753ae3cb8e186ceb030a8a37de78
Janak Mandavgade <janak2703@gmail.com>
2026-05-24 17:55:55 +0530

    added 892 B

---
+++ 892_B_Wrath.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+typedef long long int ll;
+
+template <typename t1, typename t2>
+ostream &operator<<(ostream &os, pair<t1, t2> p)
+{
+    return os << "{" << p.first << ", " << p.second << "}";
+}
+
+template <typename t>
+void printVec(vector<t> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+template <typename t, typename size_t>
+void inputVec(vector<t> &v, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+}
+
+void pLine(ll line)
+{
+    cout << "Reached at Line: " << line << endl;
+}
+
+void printVec(vector<ll> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+
+    vector<ll> v(n);
+    vector<ll> isAlive(n, 1);
+
+    inputVec(v, n);
+
+    ll cnt = 0;
+    ll curr_dead = LLONG_MAX;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (i < curr_dead)
+            cnt++;
+        curr_dead = min(curr_dead, i - v[i]);
+
+        // cout << i << " " << curr_dead << endl;
+        // printVec(isAlive);
+    }
+
+    // cout << "Enter ans here" << endl;
+    cout << cnt << endl;
+
+    return 0;
+}
+
+//  0 0 0 0 0 0 1 3 3
+//                  cd
+//.                 i
+//            |___|
+//           i - v[i]

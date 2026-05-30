f308f60bc924193a2eb2b391b0377d6785bf8209
janak <janak@forgejo.com>
2026-05-30 16:37:42 +0530

    ADDED 414 b

---
+++ 414_B_Cutting_Carrot.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+typedef long long int ll;
+
+template <typename t1, typename t2>
+ostream &operator<<(ostream &os, pair<t1, t2> p)
+{
+    return os << "{" << p.first << ", " << p.second << "}";
+}
+
+template <typename t>
+void printVec(vector<t> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+template <typename t>
+void print2dVec(vector<vector<t>> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+    {
+        for (ll j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return;
+}
+
+template <typename t, typename size_t>
+void inputVec(vector<t> &v, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+}
+
+template <typename t, typename size_t>
+void input2dVec(vector<vector<t>> &v, size_t n, size_t m)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < m; j++)
+            cin >> v[i][j];
+    }
+}
+
+void pLine(ll line)
+{
+    cout << "Reached at Line: " << line << endl;
+}
+
+void printVec(vector<ll> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    double n, a;
+    cin >> n >> a;
+
+    cout << fixed << setprecision(15);
+
+    // cout << "Const Area is:" << const_area << endl;
+    for (int i = 1; i < n; i++)
+    {
+        double req_h = (a * sqrt((i * 1.00) / (n * 1.00)));
+        cout << req_h << endl;
+    }
+
+    return 0;
+}

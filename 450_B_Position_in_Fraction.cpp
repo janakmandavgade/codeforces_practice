df9bcbb1b4d788b3e1f13db1a87a66590de48b1a
janak <janak@forgejo.com>
2026-06-02 11:01:47 +0530

    Added 450 B

---
+++ 450_B_Position_in_Fraction.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <format>
+#include <cmath>
+using namespace std;
+
+typedef long long int ll;
+
+template <typename t1, typename t2>
+ostream &operator<<(ostream &os, pair<t1, t2> p)
+{
+    return os << "{" << p.first << ", " << p.second << "}";
+}
+
+template <typename t>
+void printVec(vector<t> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+template <typename t>
+void print2dVec(vector<vector<t>> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+    {
+        for (ll j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return;
+}
+
+template <typename t, typename size_t>
+void inputVec(vector<t> &v, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+}
+
+template <typename t, typename size_t>
+void input2dVec(vector<vector<t>> &v, size_t n, size_t m)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < m; j++)
+            cin >> v[i][j];
+    }
+}
+
+void pLine(ll line)
+{
+    cout << "Reached at Line: " << line << endl;
+}
+
+void printVec(vector<ll> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << fixed << setprecision(15);
+
+    long double a, b;
+    ll c;
+    cin >> a >> b >> c;
+
+    if (a == b && c == 0)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+    else if (a == b && c != 0)
+    {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    long double remainder = fmod(a, b);
+
+    ll ans = -1;
+
+    for (int i = 1; i <= 1000; i++)
+    {
+        remainder *= 10;
+        int digit = (int)(remainder / b);
+
+        if (digit == c)
+        {
+            ans = i;
+            break;
+        }
+
+        remainder = fmod(remainder, b);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}

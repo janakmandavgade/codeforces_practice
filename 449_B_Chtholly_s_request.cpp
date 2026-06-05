0621f7c1491dc81d999669dcbc17144d2187459c
janak <janak@forgejo.com>
2026-06-05 11:23:04 +0530

    Added 418_B and 449 B

---
+++ 449_B_Chtholly_s_request.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+typedef long long int ll;
+
+template <typename t1, typename t2>
+ostream &operator<<(ostream &os, pair<t1, t2> p)
+{
+    return os << "{" << p.first << ", " << p.second << "}";
+}
+
+template <typename t>
+void printVec(vector<t> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+template <typename t>
+void print2dVec(vector<vector<t>> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+    {
+        for (ll j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return;
+}
+
+template <typename t, typename size_t>
+void inputVec(vector<t> &v, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+}
+
+template <typename t, typename size_t>
+void input2dVec(vector<vector<t>> &v, size_t n, size_t m)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < m; j++)
+            cin >> v[i][j];
+    }
+}
+
+void pLine(ll line)
+{
+    cout << "Reached at Line: " << line << endl;
+}
+
+void printVec(vector<ll> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, m;
+    cin >> n >> m;
+
+    ll cnt = 0;
+    ll ans = 0;
+    for (ll i = 1; i <= n; i++)
+    {
+        string s = to_string(i);
+        string t = s;
+        reverse(t.begin(), t.end());
+        s = s + t;
+
+        // cout << s << endl;
+        ll num = stoll(s);
+        // cout << num << endl;
+        ans += (num);
+    }
+    // cout << endl;
+    cout << ans % m << endl;
+
+    return 0;
+}

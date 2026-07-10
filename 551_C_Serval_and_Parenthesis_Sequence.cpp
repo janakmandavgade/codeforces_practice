fc7ec24ca26390d05fbf5573736ae773b8f6b1d7
janak <janak@forgejo.com>
2026-07-10 12:09:04 +0530

    Added 551 C

---
+++ 551_C_Serval_and_Parenthesis_Sequence.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+#include <stack>
+using namespace std;
+
+typedef long long int ll;
+
+template <typename t1, typename t2>
+ostream &operator<<(ostream &os, pair<t1, t2> p)
+{
+    return os << "{" << p.first << ", " << p.second << "}";
+}
+
+template <typename t>
+void printVec(vector<t> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+template <typename t>
+void print2dVec(vector<vector<t>> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+    {
+        for (ll j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return;
+}
+
+template <typename t, typename size_t>
+void inputVec(vector<t> &v, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+}
+
+template <typename t, typename size_t>
+void input2dVec(vector<vector<t>> &v, size_t n, size_t m)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < m; j++)
+            cin >> v[i][j];
+    }
+}
+
+void pLine(ll line)
+{
+    cout << "Reached at Line: " << line << endl;
+}
+
+void printVec(vector<ll> &v)
+{
+    for (ll i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    if ((n % 2 == 1) || !((s[0] == '(' || s[0] == '?') && (s[n - 1] == ')' || s[n - 1] == '?')))
+    {
+        cout << ":(" << endl;
+        return 0;
+    }
+    else
+    {
+        stack<char> st;
+        // cout << "Inside else" << endl;
+        // string ans = "";
+
+        ll openneed = n / 2, closeneed = n / 2;
+
+        for (ll i = 0; i < n; i++)
+        {
+            if (s[i] == '(')
+                openneed--;
+            else if (s[i] == ')')
+                closeneed--;
+        }
+
+        if (openneed < 0 || closeneed < 0)
+        {
+            cout << ":(" << endl;
+            return 0;
+        }
+
+        for (ll i = 0; i < n; i++)
+        {
+            if (s[i] == '?')
+            {
+                if (openneed)
+                {
+                    s[i] = '(';
+                    openneed--;
+                }
+                else
+                {
+                    s[i] = ')';
+                    closeneed--;
+                }
+            }
+        }
+
+        for (ll i = 0; i < n; i++)
+        {
+            if (s[i] == '(')
+            {
+                st.push('(');
+            }
+            else
+            {
+                if (st.empty())
+                {
+                    cout << ":(" << endl;
+                    return 0;
+                }
+                st.pop();
+            }
+
+            if (i != n - 1 && st.empty())
+            {
+                cout << ":(" << endl;
+                return 0;
+            }
+        }
+
+        if (!st.empty())
+        {
+            cout << ":(" << endl;
+            return 0;
+        }
+
+        cout << s << endl;
+    }
+
+    return 0;
+}
